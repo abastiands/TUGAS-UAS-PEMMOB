8572aee88d288538d3898af89f762a897733660b
Abas <abastiands@gmail.com>
2021-01-13 21:04:09 +0700

    first commit

---
+++ app/src/main/java/com/example/AppBiodata/MainActivity.java
@@ -0,0 +1,194 @@
+package com.example.AppBiodata;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ListView listView;
+    DBHelper helper;
+    LayoutInflater inflater;
+    View dialogView;
+    TextView Tv_NIM, Tv_Nama, Tv_TempatLahir, Tv_JK, Tv_Tanggal, Tv_Alamat;
+    Button btn_logout;
+
+    SharedPreferences sharedpreferences;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+            }
+        });
+
+        btn_logout = (Button) findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+
+        helper = new DBHelper(this);
+        listView = (ListView)findViewById(R.id.list_data);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setListView(){
+        Cursor cursor = helper.allData();
+        CustomCursorAdapter customCursorAdapter = new CustomCursorAdapter(this, cursor, 1);
+        listView.setAdapter(customCursorAdapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int i, long x) {
+        TextView getId = (TextView)view.findViewById(R.id.listID);
+        final long id = Long.parseLong(getId.getText().toString());
+        final Cursor cur = helper.oneData(id);
+        cur.moveToFirst();
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Pilih Opsi");
+
+        //Add a list
+        String[] options = {"Lihat Data", "Edit Data", "Hapus Data"};
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case 0:
+                        final AlertDialog.Builder viewData = new AlertDialog.Builder(MainActivity.this);
+                        inflater = getLayoutInflater();
+                        dialogView = inflater.inflate(R.layout.view_data, null);
+                        viewData.setView(dialogView);
+                        viewData.setTitle("Lihat Data");
+
+                        Tv_NIM = (TextView)dialogView.findViewById(R.id.tv_NIM);
+                        Tv_Nama = (TextView)dialogView.findViewById(R.id.tv_Nama);
+                        Tv_TempatLahir = (TextView)dialogView.findViewById(R.id.tv_TempatLahir);
+                        Tv_Tanggal = (TextView)dialogView.findViewById(R.id.tv_Tanggal);
+                        Tv_JK = (TextView)dialogView.findViewById(R.id.tv_JK);
+                        Tv_Alamat = (TextView)dialogView.findViewById(R.id.tv_Alamat);
+
+                        Tv_NIM.setText("NIM: " + cur.getString(cur.getColumnIndex(DBHelper.row_NIM)));
+                        Tv_Nama.setText("Nama: " + cur.getString(cur.getColumnIndex(DBHelper.row_nama)));
+                        Tv_TempatLahir.setText("Tempat Lahir: " + cur.getString(cur.getColumnIndex(DBHelper.row_tempatLahir)));
+                        Tv_Tanggal.setText("Tanggal Lahir: " + cur.getString(cur.getColumnIndex(DBHelper.row_tglLahir)));
+                        Tv_JK.setText("Jenis Kelamin: " + cur.getString(cur.getColumnIndex(DBHelper.row_jk)));
+                        Tv_Alamat.setText("Alamat: " + cur.getString(cur.getColumnIndex(DBHelper.row_alamat)));
+
+                        viewData.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        viewData.show();
+                }
+                switch (which){
+                    case 1:
+                        Intent iddata = new Intent(MainActivity.this, EditActivity.class);
+                        iddata.putExtra(DBHelper.row_id, id);
+                        startActivity(iddata);
+                }
+                switch (which){
+                    case 2:
+                        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                        builder1.setMessage("Data ini akan dihapus.");
+                        builder1.setCancelable(true);
+                        builder1.setPositiveButton("Hapus", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                helper.deleteData(id);
+                                Toast.makeText(MainActivity.this, "Data Terhapus", Toast.LENGTH_SHORT).show();
+                                setListView();
+                            }
+                        });
+                        builder1.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        AlertDialog alertDialog = builder1.create();
+                        alertDialog.show();
+                }
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setListView();
+    }
+
+    private void logout() {
+        sharedpreferences = getSharedPreferences(
+                LoginActivity.my_shared_preferences, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putBoolean(LoginActivity.session_status, false);
+        editor.putString(LoginActivity.TAG_ID, null);
+        editor.putString(LoginActivity.TAG_EMAIL, null);
+        editor.putString(LoginActivity.TAG_NAME, null);
+        editor.putString(LoginActivity.TAG_PHONE, null);
+        editor.commit();
+
+        intent = new Intent(MainActivity.this, LoginActivity.class);
+        finish();
+        startActivity(intent);
+    }
+}
